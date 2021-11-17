7908823c01e68441a7678f70bf4957dfd82bc439
Ning Sun <sunng@protonmail.com>
2021-11-17 23:40:32 +0800

    feat(web): import leaflet map

--- obmweb/src/obmweb/core.cljs
+++ obmweb/src/obmweb/core.cljs
@@ -6,7 +6,8 @@
    [obmweb.routes :as routes]
    [obmweb.views :as views]
    [obmweb.config :as config]
-   ))
+
+   ["leaflet" :as leaflet]))
 
 
 (defn dev-setup []
@@ -19,8 +20,17 @@
     (rdom/unmount-component-at-node root-el)
     (rdom/render [views/main-panel] root-el)))
 
+(defn setup-leaflet []
+  (let [map (.map js/L "map")]
+    (.setView map (clj->js [40.0822029 116.4624013]) 13)
+
+    (let [layer (.tileLayer js/L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            (clj->js {:attribution "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"}))]
+      (.addTo layer map))))
+
 (defn init []
   (routes/start!)
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
+  (setup-leaflet)
   (mount-root))
