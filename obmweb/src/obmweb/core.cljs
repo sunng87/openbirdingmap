46ee550b742ef077be8ed3a243f977ec328b36ca
Ning Sun <sunng@protonmail.com>
2021-11-18 23:27:32 +0800

    feat: use react-leaflet to render map

--- obmweb/src/obmweb/core.cljs
+++ obmweb/src/obmweb/core.cljs
@@ -7,26 +7,39 @@
    [obmweb.views :as views]
    [obmweb.config :as config]
 
-   ["leaflet" :as leaflet]))
+   ["react-leaflet" :as leaflet]))
 
 
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
 
+(defn setup-leaflet []
+  [:> leaflet/MapContainer
+   {:center [40.0822029 116.4624013]
+    :zoom 13}
+
+   [:> leaflet/TileLayer
+    {:attribution "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
+     :url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]])
+
 (defn ^:dev/after-load mount-root []
   (re-frame/clear-subscription-cache!)
   (let [root-el (.getElementById js/document "app")]
     (rdom/unmount-component-at-node root-el)
-    (rdom/render [views/main-panel] root-el)))
+    (rdom/render [views/main-panel] root-el))
+  (let [map-el (.getElementById js/document "map")]
+    (rdom/unmount-component-at-node map-el)
+    (rdom/render [setup-leaflet] map-el)))
 
-(defn setup-leaflet []
-  (let [map (.map js/L "map")]
-    (.setView map (clj->js [40.0822029 116.4624013]) 13)
+;; (defn setup-leaflet []
+;;   (let [map (.map js/L "map")]
+;;     (.setView map (clj->js [40.0822029 116.4624013]) 13)
 
-    (let [layer (.tileLayer js/L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            (clj->js {:attribution "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"}))]
-      (.addTo layer map))))
+;;     (let [layer (.tileLayer js/L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+;;                             (clj->js {:attribution "Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"}))]
+;;       (.addTo layer map))))
+
 
 (defn init []
   (routes/start!)
