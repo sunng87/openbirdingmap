ce0332eba40c0b8417fd110d70ad7a7e6dc98f9e
Ning Sun <sunng@protonmail.com>
2021-11-25 23:31:40 +0800

    feat(svr): initial import of server project

---
+++ obmserver/project.clj
@@ -0,0 +1,13 @@
+(defproject obmserver "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [info.sunng/ring-jetty9-adapter "0.16.0"]
+                 [metosin/reitit-ring "0.5.15"]
+                 [cheshire "5.10.1"]]
+  :main ^:skip-aot obmserver.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
