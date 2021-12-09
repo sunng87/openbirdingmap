3373dc7a1f70991e1e57a8f0df6dca2dbf3566ae
Ning Sun <sunng@protonmail.com>
2021-12-09 23:01:19 +0800

    feat(server): filter hotspot on locality query

--- obmserver/resources/queries.sql
+++ obmserver/resources/queries.sql
@@ -1,3 +1,3 @@
 -- :name find-localities-by-state-id :? :*
 -- :doc load localities by given state id
-SELECT * FROM obm_location WHERE state_code = :state_code;
+SELECT * FROM obm_location WHERE state_code = :state_code AND ltype = 'H';
