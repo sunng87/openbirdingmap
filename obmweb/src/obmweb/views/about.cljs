e7a1fefb6c09750af7abc2357c113fd45e3c2606
Ning Sun <sunng@protonmail.com>
2022-02-01 21:49:29 +0800

    feat(web): add content to about page

--- obmweb/src/obmweb/views/about.cljs
+++ obmweb/src/obmweb/views/about.cljs
@@ -12,6 +12,12 @@
     [:a {:href "https://sunng.info" :target "_blank"} "Ning Sun"]
     ". This website displays latest birding records from "
     [:a {:href "https://ebird.org" :target "_blank"} "ebird"]
-    "."]])
+    ". The source code, including data transformation tool, is open sourced on "
+    [:a {:href "https://github.com/sunng87/openbirdingmap" :target "_blank"} "github"]
+    "."]
+   [:h3.bp-heading "Data Version"]
+   [:ul.bp3-list
+    [:li "CN-11: Jan 2021 - Dec 2021"]
+    [:li "CN-32: Jan 2021 - Dec 2021"]]])
 
 (defmethod routes/panels :about-panel [] [about-panel])
