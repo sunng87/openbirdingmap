cff3f49955e5e1d56a837cd85e02e477861464cc
Ning Sun <sunng@protonmail.com>
2022-04-13 23:04:39 +0800

    feat(server): deps up

--- obmserver/project.clj
+++ obmserver/project.clj
@@ -3,32 +3,32 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  ;; web
-                 [info.sunng/ring-jetty9-adapter "0.17.2"]
-                 [ring/ring-core "1.9.4"]
+                 [info.sunng/ring-jetty9-adapter "0.17.5"]
+                 [ring/ring-core "1.9.5"]
                  [ring/ring-json "0.5.1"]
                  [ring/ring-defaults "0.3.3"]
-                 [metosin/reitit-ring "0.5.15"]
+                 [metosin/reitit-ring "0.5.18"]
                  [ring-cors "0.1.13"]
-                 [ring-logger "1.0.1"]
+                 [ring-logger "1.1.1"]
                  ;; database
-                 [conman "0.9.3"]
-                 [com.layerware/hugsql-core "0.5.1"]
-                 [com.layerware/hugsql-adapter-next-jdbc "0.5.1"]
-                 [org.mariadb.jdbc/mariadb-java-client "2.7.4"]
+                 [conman "0.9.4"]
+                 [com.layerware/hugsql-core "0.5.3"]
+                 [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
+                 [org.mariadb.jdbc/mariadb-java-client "3.0.4"]
                  ;; config
                  [stavka "0.7.0"]
                  ;; lifecycle
                  [mount "0.1.16"]
                  ;; logging
-                 [org.clojure/tools.logging "1.2.3"]
-                 [org.apache.logging.log4j/log4j-api "2.17.0"]
-                 [org.apache.logging.log4j/log4j-core "2.17.0"]
-                 [org.apache.logging.log4j/log4j-slf4j18-impl "2.17.0"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.apache.logging.log4j/log4j-api "2.17.2"]
+                 [org.apache.logging.log4j/log4j-core "2.17.2"]
+                 [org.apache.logging.log4j/log4j-slf4j18-impl "2.17.2"]
                  ;; web crawling
                  [clj-http "3.12.3"]
-                 [org.jsoup/jsoup "1.13.1"]
+                 [org.jsoup/jsoup "1.14.3"]
                  ;; parallel processing
                  [manifold "0.2.3"]]
   :main ^:skip-aot obmserver.core
