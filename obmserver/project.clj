7d710011f31a8ee65031771447587d874eb3d7a8
Ning Sun <sunng@protonmail.com>
2023-10-06 12:01:18 +0800

    chore: deps up

--- obmserver/project.clj
+++ obmserver/project.clj
@@ -5,33 +5,33 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  ;; web
-                 [info.sunng/ring-jetty9-adapter "0.18.3"]
-                 [ring/ring-core "1.9.6"]
+                 [info.sunng/ring-jetty9-adapter "0.30.0"]
+                 [ring/ring-core "1.10.0"]
                  [ring/ring-json "0.5.1"]
-                 [ring/ring-defaults "0.3.4"]
-                 [metosin/reitit-ring "0.5.18"]
+                 [ring/ring-defaults "0.4.0"]
+                 [metosin/reitit-ring "0.6.0"]
                  [ring-cors "0.1.13"]
                  [ring-logger "1.1.1"]
                  ;; database
                  [conman "0.9.6"]
                  [com.layerware/hugsql-core "0.5.3"]
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
-                 [org.mariadb.jdbc/mariadb-java-client "3.1.0"]
+                 [org.mariadb.jdbc/mariadb-java-client "3.2.0"]
                  ;; config
                  [stavka "0.7.0"]
                  ;; lifecycle
                  [mount "0.1.17"]
                  ;; logging
                  [org.clojure/tools.logging "1.2.4"]
-                 [org.slf4j/slf4j-api "2.0.5"]
-                 [org.apache.logging.log4j/log4j-api "2.19.0"]
-                 [org.apache.logging.log4j/log4j-core "2.19.0"]
-                 [org.apache.logging.log4j/log4j-slf4j2-impl "2.19.0"]
+                 [org.slf4j/slf4j-api "2.0.9"]
+                 [org.apache.logging.log4j/log4j-api "2.20.0"]
+                 [org.apache.logging.log4j/log4j-core "2.20.0"]
+                 [org.apache.logging.log4j/log4j-slf4j2-impl "2.20.0"]
                  ;; web crawling
                  [clj-http "3.12.3"]
-                 [org.jsoup/jsoup "1.15.3"]
+                 [org.jsoup/jsoup "1.16.1"]
                  ;; parallel processing
-                 [manifold "0.3.0"]]
+                 [manifold "0.4.1"]]
   :main ^:skip-aot obmserver.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
