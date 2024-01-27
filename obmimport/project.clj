a7a39d6d2e813b95899a7f614713de534686e1a5
Ning Sun <sunng@protonmail.com>
2024-01-27 21:12:53 +0800

    feat: deps up for obmimport

--- obmimport/project.clj
+++ obmimport/project.clj
@@ -3,23 +3,23 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/tools.logging "1.2.3"]
-                 [org.slf4j/slf4j-api "1.7.32"]
-                 [org.slf4j/slf4j-simple "1.7.32"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.slf4j/slf4j-api "2.0.11"]
+                 [org.slf4j/slf4j-simple "2.0.11"]
 
-                 [dev.weavejester/ragtime.core "0.9.0"]
-                 [dev.weavejester/ragtime.next-jdbc "0.9.0"]
-                 [org.mariadb.jdbc/mariadb-java-client "2.7.4"]
+                 [dev.weavejester/ragtime.core "0.9.3"]
+                 [dev.weavejester/ragtime.next-jdbc "0.9.3"]
+                 [org.mariadb.jdbc/mariadb-java-client "3.3.2"]
                  [environ "1.2.0"]
                  [org.clojure/data.csv "1.0.0"]
-                 [com.github.seancorfield/next.jdbc "1.2.761"]
+                 [com.github.seancorfield/next.jdbc "1.3.909"]
 
                  ;; http client
                  [clj-http "3.12.3"]
-                 [cheshire "5.10.1"]
+                 [cheshire "5.12.0"]
 
-                 [org.clojure/tools.cli "1.0.206"]]
+                 [org.clojure/tools.cli "1.0.219"]]
   :main ^:skip-aot obmimport.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
