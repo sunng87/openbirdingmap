41885c5d355f1a5607a049e042345ebc793e70b7
Ning Sun <sunning@greptime.com>
2026-09-19 19:54:07 +0800

    chore: deps up

--- obmimport/project.clj
+++ obmimport/project.clj
@@ -1,25 +1,25 @@
 (defproject obmimport "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "convert ebd data to obm sqlite db file"
+  :url "https://github.com/sunng87/openbirdingmap"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.6"]
-                 [org.clojure/tools.logging "1.3.0"]
-                 [org.slf4j/slf4j-api "2.0.17"]
-                 [org.slf4j/slf4j-simple "2.0.17"]
+                 [org.clojure/tools.logging "1.3.1"]
+                 [org.slf4j/slf4j-api "2.0.19"]
+                 [org.slf4j/slf4j-simple "2.0.19"]
 
                  [dev.weavejester/ragtime.core "0.12.1"]
                  [dev.weavejester/ragtime.next-jdbc "0.12.1"]
-                 [org.xerial/sqlite-jdbc "3.49.1.0"]
+                 [org.xerial/sqlite-jdbc "3.53.4.0"]
                  [environ "1.2.0"]
-                 [org.clojure/data.csv "1.1.0"]
+                 [org.clojure/data.csv "1.1.1"]
                  [com.github.seancorfield/next.jdbc "1.3.1118"]
 
                  ;; http client
                  [clj-http "3.13.1"]
                  [cheshire "6.2.0"]
 
-                 [org.clojure/tools.cli "1.1.230"]]
+                 [org.clojure/tools.cli "1.1.256"]]
   :main ^:skip-aot obmimport.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- obmserver/project.clj
+++ obmserver/project.clj
@@ -1,6 +1,6 @@
 (defproject obmserver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "server of openbirdingmap"
+  :url "https://github.com/sunng87/openbirdingmap"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.6"]
@@ -16,20 +16,20 @@
                  [conman "0.9.6"]
                  [com.layerware/hugsql-core "0.5.3"]
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
-                 [org.xerial/sqlite-jdbc "3.49.1.0"]
+                 [org.xerial/sqlite-jdbc "3.53.4.0"]
                  ;; config
                  [stavka "0.7.0"]
                  ;; lifecycle
                  [mount "0.1.24"]
                  ;; logging
-                 [org.clojure/tools.logging "1.3.0"]
-                 [org.slf4j/slf4j-api "2.0.17"]
-                 [org.apache.logging.log4j/log4j-api "2.26.0"]
-                 [org.apache.logging.log4j/log4j-core "2.26.0"]
-                 [org.apache.logging.log4j/log4j-slf4j2-impl "2.26.0"]
+                 [org.clojure/tools.logging "1.3.1"]
+                 [org.slf4j/slf4j-api "2.0.19"]
+                 [org.apache.logging.log4j/log4j-api "2.26.1"]
+                 [org.apache.logging.log4j/log4j-core "2.26.1"]
+                 [org.apache.logging.log4j/log4j-slf4j2-impl "2.26.1"]
                  ;; web crawling
                  [clj-http "3.13.1"]
-                 [org.jsoup/jsoup "1.21.1"]
+                 [org.jsoup/jsoup "1.23.2"]
                  ;; parallel processing
                  [manifold "0.5.0"]]
   :main ^:skip-aot obmserver.core
