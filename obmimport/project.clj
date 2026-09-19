2cfd9114cc8e03cdee53cc3eaacc6b6d3fcc2b50
Ning Sun <sunning@greptime.com>
2026-09-19 17:33:20 +0800

    feat: update clojure deps

--- obmimport/project.clj
+++ obmimport/project.clj
@@ -3,23 +3,23 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.0"]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [org.slf4j/slf4j-api "2.0.11"]
-                 [org.slf4j/slf4j-simple "2.0.11"]
+  :dependencies [[org.clojure/clojure "1.12.6"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [org.slf4j/slf4j-api "2.0.17"]
+                 [org.slf4j/slf4j-simple "2.0.17"]
 
-                 [dev.weavejester/ragtime.core "0.9.3"]
-                 [dev.weavejester/ragtime.next-jdbc "0.9.3"]
+                 [dev.weavejester/ragtime.core "0.12.1"]
+                 [dev.weavejester/ragtime.next-jdbc "0.12.1"]
                  [org.xerial/sqlite-jdbc "3.49.1.0"]
                  [environ "1.2.0"]
-                 [org.clojure/data.csv "1.0.0"]
-                 [com.github.seancorfield/next.jdbc "1.3.909"]
+                 [org.clojure/data.csv "1.1.0"]
+                 [com.github.seancorfield/next.jdbc "1.3.1118"]
 
                  ;; http client
-                 [clj-http "3.12.3"]
-                 [cheshire "5.12.0"]
+                 [clj-http "3.13.1"]
+                 [cheshire "6.2.0"]
 
-                 [org.clojure/tools.cli "1.0.219"]]
+                 [org.clojure/tools.cli "1.1.230"]]
   :main ^:skip-aot obmimport.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- obmserver/project.clj
+++ obmserver/project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.6"]
                  ;; web
-                 [info.sunng/ring-jetty9-adapter "0.35.0"]
-                 [ring/ring-core "1.12.2"]
+                 [info.sunng/ring-jetty9-adapter "0.40.4"]
+                 [ring/ring-core "1.15.5"]
                  [ring/ring-json "0.5.1"]
-                 [ring/ring-defaults "0.4.0"]
-                 [metosin/reitit-ring "0.6.0"]
+                 [ring/ring-defaults "0.7.1"]
+                 [metosin/reitit-ring "0.11.0"]
                  [ring-cors "0.1.13"]
                  [ring-logger "1.1.1"]
                  ;; database
@@ -20,18 +20,18 @@
                  ;; config
                  [stavka "0.7.0"]
                  ;; lifecycle
-                 [mount "0.1.19"]
+                 [mount "0.1.24"]
                  ;; logging
                  [org.clojure/tools.logging "1.3.0"]
-                 [org.slf4j/slf4j-api "2.0.16"]
-                 [org.apache.logging.log4j/log4j-api "2.24.0"]
-                 [org.apache.logging.log4j/log4j-core "2.24.0"]
-                 [org.apache.logging.log4j/log4j-slf4j2-impl "2.24.0"]
+                 [org.slf4j/slf4j-api "2.0.17"]
+                 [org.apache.logging.log4j/log4j-api "2.26.0"]
+                 [org.apache.logging.log4j/log4j-core "2.26.0"]
+                 [org.apache.logging.log4j/log4j-slf4j2-impl "2.26.0"]
                  ;; web crawling
-                 [clj-http "3.12.3"]
-                 [org.jsoup/jsoup "1.16.1"]
+                 [clj-http "3.13.1"]
+                 [org.jsoup/jsoup "1.21.1"]
                  ;; parallel processing
-                 [manifold "0.4.3"]]
+                 [manifold "0.5.0"]]
   :main ^:skip-aot obmserver.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
